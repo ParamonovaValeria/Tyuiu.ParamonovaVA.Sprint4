94cf604270a2f141e6c392749055bf1b682acefb
Tyuiu.ParamonovaVA@outlook.com <paramonova-05@mail.ru>
2024-12-11 03:19:54 +0500

    Add project files.

---
+++ Tyuiu.ParamonovaVA.Sprint4.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.ParamonovaVA.Sprint4.Task0.V21.Lib;
+
+namespace Tyuiu.ParamonovaVA.Sprint4.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumEvenArrEl()
+        {
+            DataService ds = new DataService();
+            int[] numArray = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+            int res = ds.GetSumEvenArrEl(numArray);
+            int numsWaitArray = 36;
+            Assert.AreEqual(numsWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.ParamonovaVA.Sprint4.Task0.V21/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ParamonovaVA.Sprint4.Task0.V21.Lib;
+
+namespace Tyuiu.ParamonovaVA.Sprint4.Task0.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Парамонова В.А. | РППб-24-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #4                                                            *");
+            Console.WriteLine("* Тема:                                                                *");
+            Console.WriteLine("* Задание #0                                                           *");
+            Console.WriteLine("* Вариант #21                                                          *");
+            Console.WriteLine("* Выполнил: Парамонова Валерия Алексеевна  | РППб-24-1                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+            int[] numArray = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+
+            Console.WriteLine("Заданный массив: " + "[{0}]", string.Join(",", numArray));
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма чётных элементов массива - " + ds.GetSumEvenArrEl(numArray));
+            Console.ReadKey();
+        }
+    }
+}
