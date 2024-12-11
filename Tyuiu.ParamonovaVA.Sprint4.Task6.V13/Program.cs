d57ea6b95a4019948f9b1e3de771d10b966e7a58
Tyuiu.ParamonovaVA@outlook.com <paramonova-05@mail.ru>
2024-12-11 05:31:31 +0500

    Task6

---
+++ Tyuiu.ParamonovaVA.Sprint4.Task6.V13/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.ParamonovaVA.Sprint4.Task6.V13.Lib;
+
+namespace Tyuiu.ParamonovaVA.Sprint4.Task6.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнила: Парамонова В.А.| РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Парамонова Валерия Алексеевна | РППб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Дан строковый массив данных используя класс Array подсчитайте количество *");
+            Console.WriteLine("*элементов, длина которых больше 4.                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            var cars = new string[] { "Ford", "Toyota", "Honda", "Chevrolet", "Mercedes", "BMW", "Audi" };
+
+            Console.WriteLine("Массив:");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(cars);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
